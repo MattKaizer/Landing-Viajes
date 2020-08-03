0e5bd71ff1545fa101902d91bf284aa1253022b2
Matías Benjamín Moreno Juárez <matt@iMac-de-Matias.home>
2020-08-03 18:39:45 +0100

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-Landing-Viajes
+# Travel blog
+
+[Link to github pages ](https://mattkaizer.github.io/Landing-Viajes/)
+
+### A simple, easy landing web to practice
+
+## Task Description
+
+It is a practice in which I made my first contact with sass for style management, gulp @ 4 for a small automation, and above all, to test the use of Flexbox to make it responsive.
+
+## Criteria
+
+* Choice of simple design (because its just for practice)
+* Knowing basic use of gulp
+* Responsiveness across various forms factors
+* Knowing basic use of sass
+* Trying paralax effect
+
+#### Summary of Tech used explained
+
+
+
+#### Functionality
+
+This example has no functionality but just learn something by doing.
+
+#### Design
+
+the design was also chosen for its simplicity
+
+#### Instructions
+~~~
+npm i -g gulp
+npm i
+gulp
+~~~
+
+#### Enum Tech used
+
+* HTML5
+* CSS3
+* SASS
+* GULP4
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,34 @@
-const   gulp = require('gulp'),
-        autoprefixer = require('gulp-autoprefixer'),
-        sass = require('gulp-sass'),
-        rename = require('gulp-rename');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
-gulp.task('sass', () => {
-    gulp.src('scss/app.scss')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sass({
-            includePaths: ['scss'].concat('node_modules/@fortawesome/fontawesome-free/scss/')
-        }))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest('css'));
+gulp.task('images', () => {
+  return gulp
+    .src(['./img/original/*.{png,jpg,svg}'])
+    .pipe(imagemin())
+    .pipe(gulp.dest('./img'));
 });
-        
-/* gulp.task('fonts', () => {
-    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
-      .pipe(gulp.dest('../blog/fonts'))
-  }) */
 
-gulp.task('watch', ['sass'], () => {
-    gulp.watch(['scss/*.scss'], ['sass']);
+gulp.task('styles', () => {
+  return gulp
+    .src(['scss/**/*.scss'])
+    .pipe(
+      plumber({
+        errorHandler: function (error) {
+          console.log(error.message);
+          this.emit('end');
+        },
+      })
+    )
+    .pipe(sass())
+    .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('css'));
 });
+
+gulp.task('default', gulp.parallel('styles', 'images'));
